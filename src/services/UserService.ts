bf3659f4a0135b447ea4d6185be5ec2fb228d2ba
gautam-629 <gautam629@gmail.com>
2024-01-16 22:20:12 +0545

    test

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,16 +3,19 @@ import { User } from "../entity/User";
 import { UserData } from "../types";
 import createHttpError from "http-errors";
 import { Roles } from "../constants";
+import bcrypt from "bcryptjs";
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
     async create({ firstName, lastName, email, password }: UserData) {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
         try {
             return await this.userRepository.save({
                 firstName,
                 lastName,
                 email,
-                password,
+                password: hashedPassword,
                 role: Roles.CUSTOMER,
             });
         } catch (error) {
