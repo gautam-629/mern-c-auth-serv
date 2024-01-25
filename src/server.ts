81e99a937317b571373c9c3ad2e4e55d4036473c
gautam-629 <gautam629@gmail.com>
2024-01-25 20:20:53 +0545

    add db connection

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,6 @@ const startServer = async () => {
     const PORT = Config.PORT;
 
     try {
-        // const err=createError(401,"you are not allowed to access this page")
-        // throw err;
         await AppDataSource.initialize();
         logger.info("Database connected successfully.");
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
