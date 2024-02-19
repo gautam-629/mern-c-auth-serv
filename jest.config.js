e98175fa7a30a30d9867c3fc5cdd417de464434c
gautam-629 <gautam629@gmail.com>
2024-02-19 12:18:50 +0545

    minor change

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     verbose: true,
-    collectCoverage:true,
-    coverageProvider:"v8",
+    collectCoverage: true,
+    coverageProvider: "v8",
     collectCoverageFrom: ["src/**/*.ts", "!tests/**", "!**/node_modules/**"],
 };
