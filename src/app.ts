377cfdde8b73131b541e2318808ffe98d98f3761
gautam-629 <gautam629@gmail.com>
2024-02-19 11:41:56 +0545

    minor change

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,15 @@ import authRouter from "./routes/auth";
 import tenantRouter from "./routes/tenants";
 import userRouter from "./routes/user";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
-
+app.use(
+    cors({
+        // todo: move to .env file.
+        origin: ["http://localhost:5173"],
+        credentials: true,
+    }),
+);
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.json());
