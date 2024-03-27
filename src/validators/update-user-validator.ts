b10a7c485300cafacd78e83ebffed4440f13d295
gautam-629 <gautam629@gmail.com>
2024-03-27 17:17:04 +0545

    resolve user update issuse

---
+++ src/validators/update-user-validator.ts
@@ -0,0 +1,31 @@
+import { checkSchema } from "express-validator";
+
+export default checkSchema({
+    firstName: {
+        errorMessage: "First name is required!",
+        notEmpty: true,
+        trim: true,
+    },
+    lastName: {
+        errorMessage: "Last name is required!",
+        notEmpty: true,
+        trim: true,
+    },
+    role: {
+        errorMessage: "Role is required!",
+        notEmpty: true,
+        trim: true,
+    },
+    email: {
+        isEmail: {
+            errorMessage: "Invalid email!",
+        },
+        notEmpty: true,
+        errorMessage: "Email is required!",
+        trim: true,
+    },
+    tenantId: {
+        errorMessage: "Tenant id is required!",
+        trim: true,
+    },
+});
